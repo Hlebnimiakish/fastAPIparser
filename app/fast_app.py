79996ef88cb35740ad9ae9de12c7ba8a7d46cb8f
hleb <hleb@hleb.com>
2023-04-26 14:30:49 +0300

    Added FastAPI Depends, scripts -> lamoda_scripts

--- app/fast_app.py
+++ app/fast_app.py
@@ -3,30 +3,39 @@
 """This module contains FastAPI app and it's APIs methods"""
 
 import json
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from app.database import CollectionHandler, DatabaseGetter
 from app.schema import ParseCategory, ParsePage
 from app.settings import settings
 from kafka_starter.kafka_producer import producer
 
-fast_app = FastAPI()
-lamoda_db_name = settings.lamoda_db_name
+app = FastAPI()
+lamoda_db_name: str = settings.lamoda_db_name
 
 
-@fast_app.post("/category_renewal/",
-               summary="Update database with current actual category map",
-               response_description="Task status")
+class DatabaseCollectionsList(DatabaseGetter):
+    def __call__(self) -> list:
+        return self.db.list_collection_names()
+
+
+lamoda_db_collections = DatabaseCollectionsList(lamoda_db_name)
+
+
+@app.post("/category_renewal/",
+          summary="Update database with current actual category map",
+          response_description="Task status")
 async def renew_category_map():
     producer.send("lamoda_category_parser", b"Category map renewal task")
     producer.flush()
     return {"Task send": "Category map renewal task send"}
 
 
-@fast_app.get("/get_category_map/",
-              summary="Returns current actual categories and subcategories",
-              response_description="Current actual categories and subcategories")
+@app.get("/get_category_map/",
+         summary="Returns current actual categories and subcategories",
+         response_description="Current actual categories and subcategories")
 async def get_category_map():
     collection = CollectionHandler(lamoda_db_name, "categories")
     categories_data = [category for category in collection.find({})]
@@ -37,18 +46,17 @@ async def get_category_map():
     return categories_map
 
 
-@fast_app.get("/available_collections/",
-              summary="Returns all available database collections list",
-              response_description="Collections list")
-async def get_collections_list():
-    db = DatabaseGetter(lamoda_db_name).db
-    collections = db.list_collection_names()
+@app.get("/available_collections/",
+         summary="Returns all available database collections list",
+         response_description="Collections list")
+async def get_collections_list(collections:
+                               Annotated[list, Depends(lamoda_db_collections)]):
     return collections
 
 
-@fast_app.put("/parse_category/",
-              summary="Takes in category_type and subcategory and sends parsing task to kafka",
-              response_description="Task status or error")
+@app.put("/parse_category/",
+         summary="Takes in category_type and subcategory and sends parsing task to kafka",
+         response_description="Task status or error")
 async def parse_category(category_params: ParseCategory):
     collection = CollectionHandler(lamoda_db_name, "categories")
     document = collection.find_one({"category_type":
@@ -63,14 +71,14 @@ async def parse_category(category_params: ParseCategory):
     return {"Task send": "Category parsing task send"}
 
 
-@fast_app.get("/view_category_goods/",
-              summary="Returns list of 100 category goods from cursor",
-              response_description="Up to 100 category goods list")
-async def view_category_goods(collection_name: str,
+@app.get("/view_category_goods/",
+         summary="Returns list of 100 category goods from cursor",
+         response_description="Up to 100 category goods list")
+async def view_category_goods(collections:
+                              Annotated[list, Depends(lamoda_db_collections)],
+                              collection_name: str,
                               page_num: int = 1,
                               goods_per_page: int = 100):
-    db = DatabaseGetter(lamoda_db_name).db
-    collections = db.list_collection_names()
     if collection_name not in collections:
         return {"Collection not found Error": "Collection not found 404"}
     collection = CollectionHandler(lamoda_db_name, collection_name)
@@ -85,10 +93,10 @@ async def view_category_goods(collection_name: str,
     return docs_data
 
 
-@fast_app.put("/parse_goods_from_page/",
-              summary="Set task to parse passed page link and adds to database all goods from"
-                      "passed page",
-              response_description="Task status or error")
+@app.put("/parse_goods_from_page/",
+         summary="Set task to parse passed page link and adds to database all goods from"
+                 "passed page",
+         response_description="Task status or error")
 async def parse_passed_page(page_link: ParsePage):
     message = json.dumps(page_link.dict()).encode('utf-8')
     producer.send("lamoda_page_parser", message)
