138b859f27b4ba5acada3553c1b1e05c116e23bb
hleb <hleb@hleb.com>
2023-05-02 17:44:38 +0300

    Dependencies update and redis cache dependency added

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,25 +1,41 @@
+# pylint: skip-file
 # pylint: disable=import-error, too-few-public-methods
 
 """This module contains common dependencies of fastAPI app"""
 
+from fastapi import BackgroundTasks, HTTPException, Request
+
 from app.database import CollectionHandler
+from app.redis_cache import CacheFactory
 
 
 class CollectionDocsViewer:
     """This class contains params and method needed to get documents from
     mongodb collection for api view"""
-    def __init__(self, collections: list, db_name: str):
+    def __init__(self, db_name: str):
         """Instanciate class with passed collections list and db_name"""
-        self.db_collections = collections
         self.db_name = db_name
 
     def __call__(self,
+                 request: Request,
+                 background_tasks: BackgroundTasks,
                  collection_name: str,
                  page_num: int = 1,
                  goods_per_page: int = 100):
-        """Returns list of collection documents paginated with passed parameters"""
-        if collection_name not in self.db_collections:
-            return [{"Collection not found Error": "Collection not found 404"}]
+        cache = CacheFactory(self.docs_getter)
+        docs = cache.cache_checker(request=request,
+                                   background_tasks=background_tasks,
+                                   collection_name=collection_name,
+                                   page_num=page_num,
+                                   goods_per_page=goods_per_page)
+        return docs
+
+    def docs_getter(self,
+                    collection_name: str,
+                    page_num: int = 1,
+                    goods_per_page: int = 100):
+        """Returns list of collection documents from database paginated
+        with passed parameters"""
         collection = CollectionHandler(self.db_name, collection_name)
         docs = collection.find(filter={},
                                projection={'id': False},
@@ -29,4 +45,32 @@ class CollectionDocsViewer:
         for doc in docs:
             doc['_id'] = str(doc['_id'])
             docs_data.append(doc)
+        if not docs_data:
+            raise HTTPException(status_code=404,
+                                detail="Collection is empty or doesn't exist")
         return docs_data
+
+
+class CategoriesMapViewer:
+    def __init__(self, db_name: str):
+        self.db_name = db_name
+
+    def __call__(self,
+                 request: Request,
+                 background_tasks: BackgroundTasks):
+        cache = CacheFactory(self.lamoda_categories_map_getter,
+                             expiration=500)
+        categories_map = cache.cache_checker(request=request,
+                                             background_tasks=background_tasks)
+        return categories_map
+
+    def lamoda_categories_map_getter(self):
+        collection = CollectionHandler(self.db_name, "categories")
+        categories_data = [category for category in collection.find({})]
+        if not categories_data:
+            raise HTTPException(status_code=404, detail="No categories found")
+        categories_map = {}
+        for category in categories_data:
+            categories_map[category['category_type']] = \
+                [key for key in category['subcategories'].keys()]
+        return categories_map
